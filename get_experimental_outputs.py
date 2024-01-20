989efe3b35955a5d552bdb5d40f776595c9a3ff7
bposehn <brendan.posehn@generalfusion.com>
2024-01-19 16:10:34 -0800

    Working tool to get TA configs from exp data

--- get_experimental_outputs.py
+++ get_experimental_outputs.py
@@ -14,14 +14,41 @@ import pandas as pd
 import numpy as np
 
 def get_experimental_table_axis_values(shot_min: int, shot_max: int):
+    table_axis_names = ['NevinsA', 'NevinsC', 'NevinsN', 'beta_pol1', 'psieq_dc', 'psieq_soak', 'CurrentRatio']
+    query_layers = ['reconstruction/' + table_axis_name + '_mean' for table_axis_name in table_axis_names]
+    query_layers += ['reconstruction/fit_quality']
+
+    num_no_recon_data_shots = 0
+    num_bad_quality_shots = 0
+    configs = []
     for shot in range(shot_min, shot_max):
-        q_options = {'experiment': 'pi3b',
-                    'manifest': 'default',
-                    'shot': shot,
-                    'layers': 'reconstruction/*/*',
+        fit_quality_options = {'experiment': 'pi3b',
+                    'manifest': 'default', 'shot': shot,
+                    'layers': query_layers,
                     'nodisplay': True}
-        data = fetch_data.run_query(q_options)
-    # TODO test this 
+        try:
+            data = fetch_data.run_query(fit_quality_options)
+        except:
+            num_no_recon_data_shots += 1
+            continue
+
+        layer_order = [layer[-1] for layer in data['layers']]
+        fit_quality_index = layer_order.index('fit_quality')
+
+        fit_qualities = list(data['waves'][fit_quality_index].data)
+        # if np.mean(fit_qualities) > 1:
+        #     num_bad_quality_shots += 1
+        #     continue
+
+        time_step_index = random.choice(range(len(fit_qualities)))
+        config = {}
+        for table_axis_name in table_axis_names:
+            config[table_axis_name] = data['waves'][layer_order.index(table_axis_name + '_mean')][time_step_index]
+
+        configs.append(config)
+
+    print(f'{num_no_recon_data_shots} shots had no recon data\n{num_bad_quality_shots} had unacceptable fit quality')
+    return configs
 
 def get_random_table_axis_configs(num_configs, table_dat_path):
 
@@ -39,9 +66,5 @@ def get_random_table_axis_configs(num_configs, table_dat_path):
     return df
 
 if __name__ == '__main__':
-    # shot_min = 18000
-    # shot_max = 
-    # shot_range = 
-    shared_file = '/home/brendan.posehn@gf.local/tables/pi3b_asbuilt_g486a_emsoak1_2023-05-19_shared'
-    df = get_random_table_axis_configs(10, shared_file)
-    df.to_csv('data/test_table_axis_configs.csv', index=False)
+    configs = get_experimental_table_axis_values(18711, 18829)
+    breakpoint()
