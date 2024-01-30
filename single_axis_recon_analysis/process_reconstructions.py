3d139ad8a1b86802933b8047bc23fca050a02c32
bposehn <brendan.posehn@generalfusion.com>
2024-01-29 17:31:33 -0800

    WIP plotting for coils and TAs from recon results

--- single_axis_recon_analysis/process_reconstructions.py
+++ single_axis_recon_analysis/process_reconstructions.py
@@ -8,6 +8,9 @@ import re
 import pandas as pd
 import numpy as np
 
+COILS_OF_INTEREST = ['Coil_A', 'Coil_C', 'Coil_D', 'PFC_1','PFC_3']
+QUERY_COLUMN_NAMES = ['WBpolFCnoDC', 'q050', 'NevinsC', 'beta_pol1']
+
 def get_coils_from_filename(filename, coil_names):
     coils = {}
     for coil_name in coil_names:
@@ -18,14 +21,12 @@ def get_coils_from_filename(filename, coil_names):
     
     return coils
 
-def get_df_from_recons(recons_dir, num_expected):
-    query_column_names = ['WBpolFCnoDC', 'q050', 'NevinsC', 'beta_pol1']
-    
-    data = np.empty((num_expected, 2*len(query_column_names)))
+def get_df_from_recons(recons_dir, num_expected):   
+    data = np.empty((num_expected, 2*len(QUERY_COLUMN_NAMES)))
 
     suffixes = ['_mean', '_truth', '_1sigma']
     df_column_names = []
-    for query_column_name in query_column_names:
+    for query_column_name in QUERY_COLUMN_NAMES:
         df_column_names += [query_column_name + suffix for suffix in suffixes]
 
     pattern = re.compile("^testcase_[0-9]+.csv")
@@ -49,30 +50,52 @@ def organize_df(recon_df: pd.DataFrame):
     out_filenames_path = ''
     out_filenames = pickle.load(open(out_filenames_path))
 
-    num_coils = 5
-    num_configs_per_coil = 10
-    num_configs_per_table_axis_config = 1 + (num_coils * num_configs_per_coil) 
-
-    data = np.empty(len(recon_df), len(recon_df.columns)+2)
+    num_coils = len(COILS_OF_INTEREST)
+    num_configs_per_coil = 10 # plus the base!
+    num_configs_per_table_axis_config = 1 + (num_coils * num_configs_per_coil)
+    data = np.empty(len(recon_df), len(recon_df.columns) + num_coils + 2) # 2 for Coil Config Index, TA Config Index
     for i_filename, filename in enumerate(out_filenames):
-        i_coil_config = int(i_filename / num_configs_per_table_axis_config)
-        i_ta_config = i_filename % num_configs_per_table_axis_config
+        i_ta_config = int(i_filename / (num_configs_per_table_axis_config + 1)) 
+        i_coil_config = i_filename % (num_configs_per_table_axis_config + 1) #in [0, 50], 0 is base 
         data[i_filename, 0] = i_coil_config
         data[i_filename, 1] = i_ta_config
+        coil_values = get_coils_from_filename(filename, COILS_OF_INTEREST)
+        for i_coil, coil_name in coil_values:
+            data[i_filename, 2 + i_coil] = coil_values[coil_name]
         row = recon_df.loc[filename] # what to do if can't get it
         data[2:] = row.values
 
-    column_names = ['Coil Config Index', 'TA Config Index'] + recon_df.columns.values.tolist()
+    column_names = ['Coil Config Index', 'TA Config Index'] + COILS_OF_INTEREST + recon_df.columns.values.tolist()
     organized_df = pd.DataFrame(data = data, columns = column_names)
 
     return organized_df
 
+def get_truth_outside_sigma_bound(organized_df: pd.DataFrame, sigma_range: float):
+
+    col_type = 'TOSB'
+    col_descriptors = [prod[0] + '_' + prod[1] for prod in itertools.product(COILS_OF_INTEREST, QUERY_COLUMN_NAMES)]
+    col_names = [col_type + coil_name for descriptor in col_descriptors]
+    data = np.empty((len(organized_df, len(COILS_OF_INTEREST))))
+    
+    ta_config_index_colname = 'TA Config Index'
+
+    #each TA config, each coil 
+    for i_ta_config in organized_df[ta_config_index_colname].unique():
+        first_i_ta_config_index = organized_df.where(organized_df[ta_config_index_colname] == i_ta_config).first_valid_index()
+        base_coil_row = pd.DataFrame(data = [organized_df.iloc[first_i_ta_config_index]], columns = organized_df.columns)
+        for coil_name in COILS_OF_INTEREST:
+            varying_coils_df = organized_df.loc[(organized_df[coil_name] != organized_df.iloc[0][coil_name]) & (organized_df[ta_config_index_colname] == first_i_ta_config_index)]
+            varying_coils_df = pd.concat([base_coil_row, varying_coils_df])
+
+
+
+
+    return f'truth outside {sigma_range} sigma', row
+
 def analyze_ta_config_set_size(organized_df):
-
-
+    pass
 
 def analyze_df(df):
-
+    pass
 
 if __name__ == '__main__':
-    get_coils_from_filename('0hz-Coil_A_0A-Coil_B_0A-Coil_C_449.048268A-Coil_D_-343.1827A-PFC_1_0A-PFC_2_0A-PFC_3_0A_s0.019_dc-0.028_a0.7_b0.7_c0.33_y29257.92357454872_i0.41_b0.10_pa2_pb02.hdf5', ['Coil_D', 'PFC_1'])
