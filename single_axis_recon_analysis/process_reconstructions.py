ab80c678673810c8472b4f346ec8e9c1f202a5bd
bposehn <brendan.posehn@generalfusion.com>
2024-02-20 15:26:51 -0800

    TSOB plots working:

--- single_axis_recon_analysis/process_reconstructions.py
+++ single_axis_recon_analysis/process_reconstructions.py
@@ -12,8 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-COILS_OF_INTEREST = ['Coil_A', 'Coil_C', 'Coil_D', 'PFC_1','PFC_3']
+COILS_OF_INTEREST = ['Coil_A', 'Coil_C', 'Coil_D', 'PFC_1','PFC_2']
 QUERY_COLUMN_NAMES = ['WBpolFCnoDC', 'q050', 'NevinsC', 'beta_pol1']
+TABLE_D_CONFIG = {  'Coil_A': 0,
+                    'Coil_B': 0,
+                    'Coil_C': 352.6,
+                    'Coil_D': -312.8,
+                    'PFC_1': -154.3,
+                    'PFC_2': -42.5}
 
 def get_coils_from_filename(filename, coil_names):
     coils = {}
@@ -26,51 +32,65 @@ def get_coils_from_filename(filename, coil_names):
     return coils
 
 def get_df_from_recons(recons_dir, num_expected):   
-    data = np.empty((num_expected, 2*len(QUERY_COLUMN_NAMES)))
-
-    suffixes = ['_mean', '_truth', '_1sigma']
+    suffixes = ['_mean', '_truth', '_sigma']
     df_column_names = []
     for query_column_name in QUERY_COLUMN_NAMES:
         df_column_names += [query_column_name + suffix for suffix in suffixes]
 
-    pattern = re.compile("^testcase_[0-9]+.csv")
+    other_recon_output_names = ['fit_quality']
+    data = np.empty((num_expected, len(df_column_names)+len(other_recon_output_names)))
+
+    pattern = re.compile("^testcase_[0-9]+_[1-5].csv")
     i_row = -1
     filenames = []
     for filepath in os.listdir(recons_dir):
+        if len(filenames)%100 == 0:
+            print(f'{100*len(filenames)/1000}% processed')
         if pattern.match(filepath):
             i_row += 1
             recon_df = pd.read_csv(os.path.join(recons_dir, filepath))
-            for i_column, query_column_name in enumerate(df_column_names):
+            for i_column, query_column_name in enumerate(df_column_names + other_recon_output_names):
                 data[i_row, i_column] = recon_df[query_column_name].item()
-            filenames.append(recon_df['FileName'])
+            filenames.append(recon_df['FileName'].item())
 
-    interim_df = pd.DataFrame(data = data, columns = df_column_names)
+    data = data[:len(filenames)]
+
+    interim_df = pd.DataFrame(data = data, columns = df_column_names + other_recon_output_names)
     interim_df['FileName'] = filenames 
     interim_df.set_index('FileName', inplace=True)
 
     return interim_df
 
-def organize_df(recon_df: pd.DataFrame):
-    out_filenames_path = ''
-    out_filenames = pickle.load(open(out_filenames_path))
+def organize_df(recon_df: pd.DataFrame, out_filenames_pickle_path, num_expected):
+    out_filenames = pickle.load(open(out_filenames_pickle_path, 'rb'))
 
     num_coils = len(COILS_OF_INTEREST)
     num_configs_per_coil = 10 # plus the base!
     num_configs_per_table_axis_config = 1 + (num_coils * num_configs_per_coil)
-    data = np.empty(len(recon_df), len(recon_df.columns) + num_coils + 2) # 2 for Coil Config Index, TA Config Index
+    data = np.empty((num_expected, len(recon_df.columns) + num_coils + 2)) # 2 for Coil Config Index, TA Config Index
     for i_filename, filename in enumerate(out_filenames):
-        i_ta_config = int(i_filename / (num_configs_per_table_axis_config + 1)) 
-        i_coil_config = i_filename % (num_configs_per_table_axis_config + 1) #in [0, 50], 0 is base 
+        filename = os.path.basename(filename)
+        i_ta_config = int(i_filename / (num_configs_per_table_axis_config)) 
+        i_coil_config = i_filename % (num_configs_per_table_axis_config) #in [0, 50], 0 is base 
         data[i_filename, 0] = i_coil_config
         data[i_filename, 1] = i_ta_config
         coil_values = get_coils_from_filename(filename, COILS_OF_INTEREST)
-        for i_coil, coil_name in coil_values:
+        for i_coil, coil_name in enumerate(coil_values):
             data[i_filename, 2 + i_coil] = coil_values[coil_name]
-        row = recon_df.loc[filename] # what to do if can't get it
-        data[2:] = row.values
+
+        if filename in recon_df.index:
+            row = recon_df.loc[filename]
+            column_values_for_filename = row.values
+        else:
+            # equil failed 
+            column_values_for_filename = [np.nan]*len(recon_df.iloc[0].values)
+        data[i_filename, 2+len(coil_values):] = column_values_for_filename
 
     column_names = ['Coil Config Index', 'TA Config Index'] + COILS_OF_INTEREST + recon_df.columns.values.tolist()
     organized_df = pd.DataFrame(data = data, columns = column_names)
+    organized_df['FileName'] = [os.path.basename(fname) for fname in out_filenames]
+
+    organized_df = organized_df.astype({'Coil Config Index': 'int32', 'TA Config Index': 'int32'})
 
     return organized_df
 
@@ -78,14 +98,14 @@ def get_truth_outside_sigma_bound(organized_df: pd.DataFrame, sigma_range: float
 
     col_type = 'TOSB'
     col_descriptors = [prod[0] + '_' + prod[1] for prod in itertools.product(COILS_OF_INTEREST, QUERY_COLUMN_NAMES)]
-    col_names = [col_type + '_' + coil_name for descriptor in col_descriptors]
+    col_names = [col_type + '_' + descriptor for descriptor in col_descriptors]
+
+    ta_config_index_colname = 'TA Config Index'
 
     num_ta_configs = len(organized_df[ta_config_index_colname].unique())
 
-    data = np.empty((len(num_ta_configs, len(col_names))))
+    data = np.empty((num_ta_configs, len(col_names)))
     
-    ta_config_index_colname = 'TA Config Index'
-
     # Entry for each table axis config, for each coil, for each col 
     filenames = []
     for i_ta_config in organized_df[ta_config_index_colname].unique():
@@ -97,50 +117,60 @@ def get_truth_outside_sigma_bound(organized_df: pd.DataFrame, sigma_range: float
             varying_coils_df = pd.concat([base_coil_row, varying_coils_df]) # this is for this ta_config_idnex
             for i_col_name, col_name in enumerate(QUERY_COLUMN_NAMES):
                 truth_values = varying_coils_df[col_name + '_truth']
-                recond_values = varying_coils_df[col_name + '_median']
-                recond_1sigmas = varying_coils_df[col_name + '_1sigma']
+                recond_values = varying_coils_df[col_name + '_mean']
+                recond_sigmas = varying_coils_df[col_name + '_sigma']
 
-                # COULD INTERPOLATE THIS
-                coil_indexes = varying_coils_df.index[(truth_values < recond_values - sigma_range * recond_1sigmas)
-                                                      | (truth_values > recond_values + sigma_range * recond_1sigmas)].tolist()
+                # z_scores = abs((truth_values - recond_values) / recond_sigmas).to_numpy()
+                # z_scores = z_scores[~np.isnan(z_scores)]
+                # b, m = np.polyfit(range(len(z_scores)), z_scores, 1)
+                # slopes.append(m)
+
+                # should also add checking that each one after does (i.e not just a middle coil point, that is concerning)
+
+                # COULD INTERPOLATE THIS ? 
+                osb_rows = varying_coils_df.loc[(truth_values < recond_values - sigma_range * recond_sigmas)
+                                                      | (truth_values > recond_values + sigma_range * recond_sigmas)]
                 
                 i_data_column = i_col_name + i_coil_name*len(QUERY_COLUMN_NAMES)
 
-                if len(coil_indexes) == 0:
+                if len(osb_rows) == 0:
                     data[i_ta_config, i_data_column] = np.nan # Represents coil range was not large enough to get to point when is oor 
+                    continue
 
-                lowest_failed_coil_index = min(coil_indexes)
-                lowest_failed_coil_deviance = varying_coils_df.iloc[lowest_failed_coil_index][coil_name] - organized_df.iloc[0][coil_name]
-        
+                lowest_failed_coil_index = min(osb_rows['Coil Config Index'])
+                lowest_failed_coil_deviance = varying_coils_df.loc[varying_coils_df['Coil Config Index'] == lowest_failed_coil_index][coil_name] \
+                                                - organized_df.iloc[0][coil_name]
+
                 data[i_ta_config, i_data_column] = lowest_failed_coil_deviance
-
-    df = pd.DataFrame(data, col_names)
+    
+    df = pd.DataFrame(data, columns=col_names)
 
     return df
 
 def plot_truth_outside_sigma_bound(TOSB_df):
 
     num_columns = 3
-    medians = []
-    fig, axs = plt.subplots(len(COILS_OF_INTEREST), num_columns)
+    fig, axs = plt.subplots(int(np.ceil(len(COILS_OF_INTEREST)/num_columns)), num_columns)
     query_column_colors = list(mcolors.BASE_COLORS.keys())[:len(QUERY_COLUMN_NAMES)]
     for i_coil, coil_name in enumerate(COILS_OF_INTEREST):
+        medians = []
         ax = axs[int(i_coil/num_columns), i_coil%num_columns]
         for i_col_name, col_name in enumerate(QUERY_COLUMN_NAMES):
             vals = TOSB_df['TOSB_' + coil_name + '_' + col_name]
             medians.append(vals.median())
-            ax.hist(vals, label=col_name, histtype='step', color=query_column_colors[i_col_name])
-        ax.axvline(x = min(medians), c=query_column_colors[np.argmin(medians)])
+            # ax.hist(vals, label=col_name, histtype='step', color=query_column_colors[i_col_name])
+            ax.hist(vals, label=col_name, alpha = .5, color=query_column_colors[i_col_name])
+        # ax.axvline(x=min(medians), c=query_column_colors[np.argmin(medians)])
         ax.set_xlabel('Coil Deviation (A)')
         ax.legend()
         ax.set_title(coil_name)
-
+    plt.suptitle(f'Truth Outside of Coil Varied Reconstructed $\mu \pm 1\sigma$\nTable D: {TABLE_D_CONFIG}')
     plt.show()
 
 def plot_test_set_size_analysis(organized_df: pd.DataFrame):
     #for each coil, for each col, show that avg error reaches asymptote
 
-    num_testcases = 200
+    num_testcases = 195
     num_testcases_per_iteration = np.arange(40, num_testcases+1, 20, dtype=int) # can be used as i_ta_config idxs
     i_ta_configs = np.arange(0, num_testcases, 1, dtype=int)
     random.shuffle(i_ta_configs)
@@ -155,17 +185,45 @@ def plot_test_set_size_analysis(organized_df: pd.DataFrame):
         coil_config_df = organized_df.loc[organized_df['Coil Config Index'] == i_coil_config_to_test]
         ax = axs[int(i_i_coil_config/num_columns), i_i_coil_config%num_columns]
         for col_name in QUERY_COLUMN_NAMES:
+            # if col_name == 'NevinsC':
+            #     continue
             vals = []
             for i_ta_configs_in_iteration in i_ta_configs_per_iteration:
                 df = coil_config_df.loc[coil_config_df['TA Config Index'].isin(i_ta_configs_in_iteration)]
-                vals.append(abs((df[col_name + '_truth'] - df[col_name + '_mean']) / df[col_name + '_1sigma']).mean())
+                df = df.loc[df[col_name + '_sigma'] > 1e-6] # confirmed this is only for high pfc changes
+
+                z_scores = (df[col_name + '_truth'] - df[col_name + '_mean']) / df[col_name + '_sigma'] # if use this, some sigmas are 1e-2, some are 1e-15? 
+                # z_scores = (df[col_name + '_truth'] - df[col_name + '_mean']) / df[col_name + '_truth'] # TODO not z scores
+                vals.append(np.mean(z_scores[np.isfinite(z_scores)])) # Looks fine with medians but not means, large outliers exist
             ax.plot(num_testcases_per_iteration, vals, label = col_name)
         ax.set_xlabel('Number of testcases')
         ax.set_ylabel('Z Score')
         ax.legend()
         ax.set_title(coil_config_names[i_i_coil_config])
 
+    plt.suptitle('Validate Test Set Size')
+    # plt.savefig('plots/validate_test_set_size.png')
+
     plt.show()
 
 if __name__ == '__main__':
-    pass
+    num_expected=9945
+
+    plt.style.use('dark_background')
+
+    # recons_df = get_df_from_recons('data/recon_results', num_expected)
+    # recons_df.to_csv('data/recon_results.csv')
+
+    recons_df = pd.read_csv('data/recon_results.csv')
+    recons_df.set_index('FileName', inplace=True)
+    organized_df = organize_df(recons_df, 'out_files.pickle', num_expected)
+        
+    # plot_test_set_size_analysis(organized_df)
+
+    # tosb_df = get_truth_outside_sigma_bound(organized_df, sigma_range=1)
+    # tosb_df.to_csv('data/tosb_results.csv')
+
+    tosb_df = pd.read_csv('data/tosb_results.csv')
+
+    plot_truth_outside_sigma_bound(tosb_df)
+    breakpoint()
