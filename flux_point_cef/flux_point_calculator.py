Fixed bug in equator normalizing

flux_point_cef/flux_point_calculator.py
@@ -15,8 +15,8 @@ class FluxPointCalculator():
         coil_names = list(coil_config.keys())
         coil_values = np.array([coil_config[coil_name] for coil_name in coil_names])[np.newaxis].T
 
-        unnormd_point_flux_values = self.flux_per_amp_df[coil_names] @ coil_values
-        point_flux_values = unnormd_point_flux_values / unnormd_point_flux_values['equator']
+        unnormd_point_flux_values: pd.Series = self.flux_per_amp_df[coil_names] @ coil_values
+        point_flux_values = unnormd_point_flux_values / unnormd_point_flux_values.loc['equator']
 
         return point_flux_values.to_dict()[0]
     